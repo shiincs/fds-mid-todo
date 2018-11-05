47589f814bbe537fb0712076ce1e8adb9b1a6da3
Shin Changseon <skywing27@gmail.com>
2018-11-05 15:38:40 +0900

    feat: login request success

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://pond-client.glitch.me/'
+})
 
 const templates = {
   loginForm: document.querySelector('#login-form').content
@@ -15,7 +20,19 @@ function drawLoginForm() {
 
   loginFormEl.addEventListener('submit', async e => {
     e.preventDefault()
-    alert('로그인 요청 전송')
+    // e: 이벤트 객체
+    // e.target: 이벤트를 실제로 일으킨 요소 객체(여기선 loginFormEl)
+    // e.target.elements: 폼 내부에 들어있는 요소 객체를 편하게 가져올 수 있는 특이한 객체
+    // e.target.elements.username: 폼 내부에 들어 있는 요소들 중 name 속성값이 "username"인 요소 객체
+    // e.target.elements.username.value: 사용자가 요소 객체에 입력한 값
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
+
+    const res = await api.post('/users/login', {
+      username: username,
+      password: password
+    })
+    alert(res.data.token)
   })
 
   // 3. 문서 내부에 삽입하기
